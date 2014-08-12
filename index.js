89793ec3d81451c19371077c2d2dfcb7d0aa7064
Mike <m@thecoded.com>
2014-08-12 15:16:16 -0400

    working follow bots

--- index.js
+++ index.js
@@ -52,6 +52,72 @@ var MongoClient1 = require('mongodb').MongoClient;
 //global functs
 
 
+function queueJob(data, theCallback, waitTime, numTimes){
+
+    whichTime=0;
+    var originalWaitTime= waitTime;
+    for(w=0; w<numTimes; w++){
+
+        //which callback element in the data array we're on
+        
+        setTimeout(function(){
+
+            theCallback(data[whichTime]);
+       
+
+        }, waitTime, whichTime);
+        console.log('\n \nwaittime='+waitTime);
+        whichTime=whichTime+1;
+        waitTime=waitTime+originalWaitTime;
+    }
+  
+
+}
+
+
+function queueJobForBot(data, theCallback, waitTime, numTimes, whichBot){
+
+    var whichTime=0;
+    var originalWaitTime= whichBot.currentWaitTime;
+    if(data.length==0){
+        return false;
+    }
+    if(numTimes > data.length){
+        numTimes = data.length;
+    }
+
+    var thatData= data;
+        var  whichO=0;
+        //which callback element in the data array we're on
+        var sendArr={};
+    for(w=0; w<numTimes; w++){
+       
+        sendArr['whichOne']= whichO; 
+
+        setTimeout(function(){
+            console.log(whichO);
+            //console.log(whichBot.onGroup);
+           theCallback(thatData, whichBot);
+       
+
+        }, whichBot.currentWaitTime, thatData);
+       
+        whichO= whichO+1;
+       // console.log(whichO);
+      console.log('\n \nwaittime='+whichBot.currentWaitTime);
+        whichTime=whichTime+1;
+
+        if(whichBot.onAction >= whichBot.defaultActionLimit){
+            console.log("reached default limit for user:"+ whichBot.username);
+            return false;
+        }
+        whichBot.onAction= whichBot.onAction+1;
+        whichBot.currentWaitTime=whichBot.defaultWaitTime+whichBot.currentWaitTime;
+        waitTime=waitTime+originalWaitTime;
+    }
+  
+    return true;
+}
 
 // Utility function that downloads a URL and invokes
 // callback with the data.
@@ -134,7 +200,7 @@ function getFollowers(postUrl, keyword){
               	newArr.push(newElem)
               });
 
-
+              console.log(newArr);
                dbv.collection('users').insert( newArr,function(err, records){
              			console.log("user saved saved");
 
@@ -167,7 +233,7 @@ function likeAndSave(postId){
 
 
 
-findPopularContent('tech');
+//findPopularContent('tech');
 
 
 var SampleApp = function() {
@@ -288,6 +354,15 @@ self.app.listen(self.port, self.ipaddress, function() {
             
             console.log('%s: Node server started on %s:%d ...',
                         Date(Date.now() ), self.ipaddress, self.port);
+       
+
+
+        //test
+   
+      //  exUser= new followBot();
+      
+        //exUser.findPopularContent('rock');
+
         });
 
  
@@ -314,6 +389,18 @@ self.app.listen(self.port, self.ipaddress, function() {
       
           
 
+    self.routes['/test'] = function(req, res){
+
+
+        exUser= new followBot();
+        //findPopularContent('blues');
+        exUser.findPopularContent('blues');
+        res.send("tested");
+
+    }
+
+
+
     self.routes['/api/scheduleTumblr']= function(req, res){
 
 
@@ -327,7 +414,7 @@ rule.minute = new schedule.Range(0, 59, 20);
 var k = schedule.scheduleJob(rule, function(){
     console.log('starting timer');
     
-   
+   /*
     dbv.close();
   MongoClient.connect('mongodb://'+connection_string, function(err, db) {
 
@@ -340,7 +427,7 @@ var k = schedule.scheduleJob(rule, function(){
      //do something
   }, 4000);
 
-
+*/
 });
   res.send('scheduled');
     };
@@ -370,3 +457,406 @@ var k = schedule.scheduleJob(rule, function(){
 var zapp = new SampleApp();
 zapp.initialize();
 zapp.start();
+
+
+
+
+var followBot= function(settings){
+
+
+
+    if(typeof(settings)=="undefined"){
+
+        settings=[];
+    }
+
+    try{
+
+       //this.inst =this;
+       settings['inst']= this;
+       this.key = airM.addBot(settings);
+
+
+
+
+    }
+
+    catch(err){
+
+        console.log("\n\n no air master set. Setting up one for you!!!\n \n");
+
+        airM = new airMaster();
+        settings['inst']= this;
+       this.key = airM.addBot(settings);
+
+    }
+    //per minute
+
+    this.currentKeyword= "fashion";
+
+    this.set = settings;
+
+    this.username=null;
+
+
+    this.defaultActionLimit=60;
+
+    this.defaultFollowLimit = 10;
+
+    //seconds... time between actions
+    this.defaultTimeSplit = 2;
+
+    //per minute
+    this.defaultRepostLimit = 10;
+
+    //per minute
+    this.defaultLikeLimit = 10;
+
+    //per minute
+    this.defaultUnFollowLimit = 1;
+
+    this.defaultWaitTime= 3000;
+
+    //status
+    this.numberFollowed = 0;
+
+    this.numberLiked=0;
+
+    this.numberReposted=0;
+
+    //how many users this user has processed this session
+    this.onUser=0;
+    this.onAction=0;
+
+    //current batch size from pool
+
+    this.batchSize=0;
+
+    //current wait time for posts/actions
+
+
+    this.currentWaitTime=0;
+
+
+    //list of usernames we've done action to.
+    this.usersDoneThisSession =[];
+
+    //to be filled in with tumblr
+    this.client=null;
+
+     this.tumblrAuth={
+              consumer_key: 'SqFu4wiBMiyfl6v40GBfCOEDEOECxtIn3A1q5z2sirVnqpifZ8',
+              consumer_secret: 'I8QizfU4HxDLujUBifDxLicvCn48S3zvn5tf2ewMuTQ8S7CLux',
+              token: 'iudo13ly6v7puDysKqpc50fUiU0hJ9MYZULOsYUabN1A3gxnAz',
+              token_secret: 'Q71jROnuApeAaaabzQC6yWfCwxaur3XhMDrjkJCQEur9QWd8l8'
+        };
+
+    //validation
+    if(typeof(settings['userCred']) !="undefined"){
+
+        this.userCred= settings['userCred'];
+
+    }
+    else{
+
+        this.userCred=[];
+
+        this.userCred['tumblr'] = tumblr.createClient(this.tumblrAuth);
+    }
+
+
+
+
+    //for tumblr to start
+    this.client = this.userCred['tumblr'];
+
+    //this.tumblrUser =  tumblr.User(this.tumblrAuth);
+
+
+    console.log('got user');
+
+    this.getListOfUsers= function(){
+
+        //based on user's settings, query the category index and set this user up
+
+
+    }
+
+
+    this.followUsers= function(usernames, whichBot){
+
+        //follow this user
+        console.log(usernames);
+
+        var randNum = Math.floor((Math.random() * usernames.length));
+
+        for(i in usernames){
+
+
+            if(whichBot.isAlreadyDone(usernames[i]['username'], whichBot)){
+                console.log("already did something to user " +usernames[i]+" this session... continuing....");
+                continue;
+            }
+            else{
+                whichBot.usersDoneThisSession.push(usernames[i]['username']);
+                console.log(whichBot.usersDoneThisSession);
+                 whichBot.followUser(usernames[i], whichBot);
+            
+            }
+
+           
+        }
+        
+        console.log('got em');
+
+    }
+
+    this.followUser= function(tUser, whichBot){
+
+        var randNum = Math.floor((Math.random() * 5000));
+        var userToFollow = tUser.username;
+       var tiUser=tUser;
+        setTimeout(function(){
+
+                   
+                        whichBot.client.follow(tiUser['username']+".tumblr.com", function(){console.log("yea")});
+                         console.log("\n\n the blog name:"+tiUser['username']);
+
+                        console.log("followed!");
+                       /* data.user.blogs.forEach(function (blog) {
+                           
+                        });*/
+                   
+                     console.log('following user'+tiUser['username']);
+                 
+
+        }, randNum, tiUser);
+    }
+
+    this.reblogPost= function(postId){
+
+        //reblog this post
+    }
+
+    this.likePost= function(postId){
+
+
+    }
+
+    this.unfollowUser= function(username){
+
+
+    }
+
+
+
+    //support functions
+
+
+        this.findPopularContent=function(keyword){
+
+
+
+            this.currentKeyword = keyword;
+            console.log(this.currentKeyword);
+            this.client.tagged(keyword, this.prepareFollowers);
+        //searches tumblr for the keyword
+        //for all post, it finds the user who posted it
+
+        }
+
+        this.prepareFollowers= function(err, data) {
+
+
+        // dbv.collection("posts").ensureIndex ("post_url", {unique: true}, function(){})
+        /*
+                dbv.collection('posts').insert( data,function(err, records){
+                 console.log("Blog Posts saved");
+
+                })
+
+            */  
+               var usersArr= [];
+
+                var theBot= airM.getCurrentBot();
+                theBot.batchSize = data.length;
+                var keyword = theBot.currentKeyword;
+                for(i in data){
+
+                    
+
+                  
+
+                  theBot.getFollowers(data[i]['post_url'], keyword, theBot.gotFollowersCB, theBot)
+                }
+
+       // ...
+            }
+
+
+
+        this.isAlreadyDone=function(username11, whichBot){
+
+
+            console.log("length is \n \n "+whichBot.usersDoneThisSession.length);
+            for(t=0; t<whichBot.usersDoneThisSession.length; t++){
+
+                if(whichBot.usersDoneThisSession[t]==username11){
+                    return true;
+                }
+            
+            
+        }
+
+        return false;
+
+    }
+     
+
+
+        this.getFollowers= function(postUrl, keyword, gotFollowersCB, whichBot){
+
+
+            
+                this.download(postUrl, function(data) {
+                
+                    // console.log(data);
+                    var $ = cheerio.load(data);
+
+            
+                    newArr= [];
+                      $(".notes .note span a").each(function(i, e) {
+                        //console.log($(this).html())
+
+                        newElem = {"username":$(this).html(), "keyword":whichBot.currentKeyword}
+                        newArr.push(newElem)
+                      });
+
+                      if(newArr==[]){
+
+                        console.log('hey... no followers for this user... returning');
+                            return false;
+                      }
+
+
+                     // console.log("made it?");
+                      whichBot.gotFollowersCB(newArr, whichBot);
+
+                      //console.log(newArr);
+                      /*
+                       dbv.collection('users').insert( newArr,function(err, records){
+                                console.log("user saved saved");
+
+                        })
+                        */
+
+
+                    })
+
+        }
+
+        this.gotFollowersCB= function(followersArr, whichBot){
+
+           console.log(followersArr);
+
+          var theData= followersArr;
+          var theCallback = whichBot.followUsers;
+          var waitTime=3000;
+           var numTimes=whichBot.defaultFollowLimit;
+
+           if(theData==[]){
+
+            console.log('no followers for this user... returning');
+            return false;
+           }
+           queueJobForBot(theData, theCallback, waitTime, numTimes, whichBot);
+           
+
+        }
+
+
+
+
+        this.download=function(url, callback) {
+              http.get(url, function(res) {
+                var data = "";
+                res.on('data', function (chunk) {
+                  data += chunk;
+                });
+                res.on("end", function() {
+                  callback(data);
+                });
+              }).on("error", function() {
+                callback(null);
+              });
+        }
+
+
+
+
+
+}
+
+
+
+
+ var airMaster = function(){
+
+    this.botsDeployed=0;
+
+    this.allBots =[];
+
+
+    this.init = function(){
+
+        this.botsDeployed=0;
+        this.allBots = [];
+
+    }
+     this.reset = function(){
+
+        this.botsDeployed=0;
+        this.allBots = [];
+        
+    }
+
+    this.getCurrentBot = function(){
+
+        var index= this.botsDeployed-1;
+        return this.allBots[index]['obj'];
+    }
+    this.addBot = function(botInfo){
+        if(typeof botInfo =="undefined"){
+            botInfo=[];
+        }
+        botInfo['id']= this.genId(10);
+        botInfo['obj']= botInfo['inst'];
+        this.botsDeployed= this.botsDeployed+1;
+        this.allBots.push(botInfo);
+        return(botInfo['id']);
+
+    }
+
+    this.subBot = function(){
+
+        this.allBots.pop();
+         this.botsDeployed- this.botsDeployed-1;
+
+    }
+
+    this.genId = function(limit){
+
+
+            var text = "";
+            var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+            for( var i=0; i < limit; i++ )
+                text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+            return text;
+
+            }
+
+
+    }
+
